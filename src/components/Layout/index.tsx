494c71429b85e1202e136f178c72bbc2fa98a967
Joonyoung <aimzero9303@gmail.com>
2023-01-30 23:44:31 +0900

    install typeScript & emotion

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,11 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+
+export default function Layout() {
+  return (
+    <>
+      <Footer />
+      <Header />
+    </>
+  );
+}
