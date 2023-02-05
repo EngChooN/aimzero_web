fa90766e74efd56ea0f81d77bd7711b52193e150
Joonyoung <aimzero9303@gmail.com>
2023-02-05 23:25:21 +0900

    landing structure dev..

---
+++ src/common/ts/landing.ts
@@ -0,0 +1,9 @@
+export const landingData = [
+  {
+    section: 1,
+    img: "images/landing/IMG_7604.jpeg",
+    title: "Who I am?",
+    content: "just human",
+    highlight: "Joonyoung Cho",
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,49 @@
 import React from "react";
 import styled from "@emotion/styled";
-import Slider from "react-awesome-slider";
-import "react-awesome-slider/dist/styles.css";
+import { landingData } from "../common/ts/landing";
+
+const Wrapper = styled.main`
+  max-width: 1200px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Section01 = styled.section`
+  display: flex;
+`;
+
+const Left = styled.div`
+  width: 50%;
+`;
+
+const Img = styled.img`
+  width: 100%;
+`;
+
+const Right = styled.div`
+  width: 50%;
+`;
+
+const Title = styled.h1``;
+
+const Content = styled.p``;
+
+const Highlight = styled.b``;
 
 export default function Home() {
   return (
-    <Slider bullets={false}>
-      <div data-src="path/to/image-1.jpg" />
-      <div data-src="path/to/image-2.jpg" />
-      <div data-src="path/to/image-3.jpg" />
-    </Slider>
+    <Wrapper>
+      <Section01>
+        <Left>
+          <Img src={landingData[0].img} />
+        </Left>
+        <Right>
+          <Title>{landingData[0].title}</Title>
+          <Content>{landingData[0].content}</Content>
+          <Highlight>{landingData[0].highlight}</Highlight>
+        </Right>
+      </Section01>
+    </Wrapper>
   );
 }
