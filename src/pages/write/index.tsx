06641ed5dea339f568ecfe0a5f3a5abd16b30aa9
Joonyoung <aimzero9303@gmail.com>
2023-03-01 02:59:37 +0900

    fix(write): 2023-03-01 self is not defined error

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -1,4 +1,12 @@
-import BoardWrite from "../../components/Main/Board/Write/BoardWrite";
+import dynamic from "next/dynamic";
+// import BoardWrite from "../../components/Main/Board/Write/BoardWrite";
+
+const BoardWrite = dynamic(
+  () => import("../../components/Main/Board/Write/BoardWrite"),
+  {
+    ssr: false,
+  }
+);
 
 export default function BoardWritePage() {
   return <BoardWrite />;
