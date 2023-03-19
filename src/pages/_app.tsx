b6532f0d9d7f2a150b379a2abcae46bb22ead3fe
Joonyoung <aimzero9303@gmail.com>
2023-03-20 00:04:52 +0900

    fix: 2023-03-20 delete locale button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
 import { appWithTranslation } from "next-i18next";
-// import LocaleButton from "../components/LocaleButton/LocaleButton";
 // recoil
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -13,7 +12,6 @@ function MyApp({ Component, pageProps }) {
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <Layout>
-          {/* <LocaleButton /> */}
           <Component {...pageProps} />
         </Layout>
       </RecoilRoot>
