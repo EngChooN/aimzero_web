3601455c03144dea7687df6a0c3e7d00d34a5381
Joonyoung Cho <aimzero@vertigogames.com>
2023-03-28 17:35:36 +0900

    fix(about): 2023-03-28 fix modal width px => %

--- src/components/PdfViewer/PdfViewer.tsx
+++ src/components/PdfViewer/PdfViewer.tsx
@@ -4,9 +4,9 @@ export default function (props) {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
   return (
     <Document
-      file={`images/about/pdf/${props.url}`} // yout pdf url
+      file={`images/about/pdf/${props.url}`} // your pdf url
     >
-      <Page height={800} pageNumber={1} />
+      <Page pageNumber={1} />
     </Document>
   );
 }
