e1e52455a59dbea320914b5ae39c07889c022bf6
Joonyoung <aimzero9303@gmail.com>
2023-09-18 14:04:59 +0900

    chore: Photo 페이지 수정
    스켈레톤 디자인과 일체감 있는 디자인 때문에,
    이미지 상자 bodrder-radius 4px 동일하게 적용 작업 함.
    
    그 외, 상단 profile information 컴포넌트 제거

--- src/components/Photo/Photo.presenter.tsx
+++ src/components/Photo/Photo.presenter.tsx
@@ -1,13 +1,9 @@
 import * as Photos from "./Photo.styles";
-import profileData from "../../../public/locales/en/common.json";
 // antd
 import { Image } from "antd";
 import { Skeleton } from "antd";
 // icon
 import { AiOutlinePlus, AiFillDelete } from "react-icons/ai";
-import { FiMail, FiInstagram } from "react-icons/fi";
-import { FaBlog } from "react-icons/fa";
-import { RxGithubLogo } from "react-icons/rx";
 import { PhotoType } from "@/types/interfaces/common";
 import { DocumentData } from "firebase/firestore";
 import React from "react";
@@ -29,16 +25,16 @@ export default function PhotoUI(props: PhotoType) {
     } = props;
 
     let width = "230px";
+
+    if (windowWidth <= 600) {
+        width = "200px";
+    }
+    if (windowWidth <= 460) {
+        width = "160px";
+    }
+
     const skeleton = () => {
         const skeletonUi = [];
-
-        if (windowWidth <= 600) {
-            width = "200px";
-        }
-        if (windowWidth <= 460) {
-            width = "160px";
-        }
-
         for (let i = 0; i < 7; i++) {
             skeletonUi.push(
                 <Skeleton.Image
@@ -55,49 +51,6 @@ export default function PhotoUI(props: PhotoType) {
 
     return (
         <Photos.Wrapper>
-            <Photos.TopSection>
-                <Photos.ProfileBox>
-                    <Photos.ProfileImg
-                        alt="profile"
-                        src={"/images/photo/profile.GIF"}
-                    />
-                    <Photos.InfoBox>
-                        <Photos.Name>Joonyoung Cho</Photos.Name>
-                        <Photos.Posts></Photos.Posts>
-                        <Photos.Info>
-                            <FiMail style={{ marginRight: "10px" }} />{" "}
-                            {profileData.photo.profileInfo.mail}
-                        </Photos.Info>
-                        <Photos.Info>
-                            <FaBlog style={{ marginRight: "10px" }} />{" "}
-                            <a
-                                href={profileData.photo.profileInfo.blog}
-                                target="_blank"
-                            >
-                                {profileData.photo.profileInfo.blog}
-                            </a>
-                        </Photos.Info>
-                        <Photos.Info>
-                            <RxGithubLogo style={{ marginRight: "10px" }} />{" "}
-                            <a
-                                href={profileData.photo.profileInfo.github}
-                                target="_blank"
-                            >
-                                {profileData.photo.profileInfo.github}
-                            </a>
-                        </Photos.Info>
-                        <Photos.Info>
-                            <FiInstagram style={{ marginRight: "10px" }} />{" "}
-                            <a
-                                href={profileData.photo.profileInfo.instagram}
-                                target="_blank"
-                            >
-                                {profileData.photo.profileInfo.instagram}
-                            </a>
-                        </Photos.Info>
-                    </Photos.InfoBox>
-                </Photos.ProfileBox>
-            </Photos.TopSection>
             <Photos.BottomSection>
                 {isLoading == true ? (
                     <>{skeleton()}</>
@@ -148,16 +101,21 @@ export default function PhotoUI(props: PhotoType) {
                             src={el.imgUrl}
                             alt="daily"
                             loading="lazy"
-                            style={{ objectFit: "cover" }}
+                            style={{ objectFit: "cover", borderRadius: "4px" }}
                             // 데이터를 불러오고, 이미지가 완전히 로드되 기 전에 표시
                             placeholder={
                                 <div
                                     style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
                                         width: `${width}`,
                                         height: `${width}`,
                                         backgroundColor: "grey",
                                     }}
-                                ></div>
+                                >
+                                    <p style={{ color: "white" }}>wait..</p>
+                                </div>
                             }
                         />
                         {userInfo?.email === "aimzero9303@gmail.com" ? (
